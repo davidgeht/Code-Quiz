22f5893359a1e257ea46b2fc8d9d0870e67cbd54
David Gehtman <davidgehtman.91@gmail.com>
2019-10-15 13:53:03 -0400

    updated questions.js
    added all the questions for the code quiz

---
+++ javascript/questions.js
@@ -0,0 +1,32 @@
+// file will hold the question arrays
+var questions = [
+    { title: "Arrays can hold the following data types:",
+      choices: ["Strings", "Numbes","Booleans","All of the above"],
+      answer: "All of the above"
+    },
+    {
+        title: "What does === operator do ?",
+        choices:["equates two data types based on value and type", "equates two data types nased on value","makes a tunnel for code to escape","Error it doesnt exist"],
+        answer: "equates two data types based on value and type"
+    },
+    {
+        title:" which are the correct pop up boxes available in javascript",
+        Choices:["alert, confirm, prompt","alert,prompt,confirm,banner","alert,prompt","alert"],
+        answer: "alert, confrim, prompt"
+    },
+    {
+        title:"In which html element do we store our javascript ?",
+        choices:["<script>","<javascript>","<javascripts>","<script></script>"],
+        answer: "<script></script>"
+    },
+    {
+        title:"What is the correct syntax for referring to an external script called script.js",
+        choices:["<script = script.js>","<script href='script.js>","<javascript src='script.js'", "<script src='script.js'>"],
+        answer:"<script src='script.js'>"
+    },
+    {
+        title: "how do you write the statement 'if x equals to 5, alert x is at 5' in javascript",
+        choices:["if (x=5){alert('x is at 5')", "if x=5: alert ! x is at 5!", "if (x=5){alert('x is at 5')}","(if x=5){alert('x is at 5')}"],
+        answer: "if (x=5){alert('x is at 5')}"
+    }
+];
